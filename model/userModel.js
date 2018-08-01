4d62610b12fa02c63ee6d2d3e560deb84cf2d2d3
Naga Gopi Raju Nallagonda <nagagopi401142@gmail.com>
2018-08-01 19:51:50 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -22,14 +22,11 @@ exports.User = sequelize.define('user', {
         allowNull: false,
         validate: {
             isEmail: true,
-            // is: ["^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,6})$"],
         },
         get() {
             return this.getDataValue('email');
         }
     },
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
 }, {
     getterMethods: {
         fullName() {
@@ -41,11 +38,11 @@ exports.User = sequelize.define('user', {
         fullName(value) {
             const names = value.split(' ');
 
-            this.setDataValue('firstname', names.slice(0, -1).join(' '));
-            this.setDataValue('lastname', names.slice(-1).join(' '));
+            this.setDataValue('firstName', names.slice(0, -1).join(' '));
+            this.setDataValue('lastName', names.slice(-1).join(' '));
         },
     }
 },{
     tableName: 'users',
-    initialAutoIncrement: 1
+    timestamps: true,
 });
